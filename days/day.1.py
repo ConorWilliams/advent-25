64e4bf21f478277457436a38b4dd507390d2c0a5
Conor <conor@myrtle.com>
2025-12-01 13:19:16 +0000

    correct

---
+++ days/day.1.py
@@ -0,0 +1,24 @@
+def main():
+    with open("data/data.1.txt", "r") as f:
+        data = f.readlines()
+        data = [d.strip().replace("R", "+").replace("L", "-") for d in data]
+        data = list(map(int, data))
+
+    start = 50
+    count = 0
+
+    for elem in data:
+        for _ in range(abs(elem)):
+            if elem > 0:
+                start = (start + 1) % 100
+            else:
+                start = (start - 1) % 100
+
+            if start == 0:
+                count += 1
+
+    print("Count:", count)
+
+
+if __name__ == "__main__":
+    main()
